5b67f26f46c2e0d8f34dd50ee5e76c4a4b46ece0
shahanaj <shahanaj89@outlook.com>
2020-03-25 08:07:50 +0600

    all change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule,FormGroup,ReactiveFormsModule } from '@angular/forms';
+import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './Employee/employees/employees.component';
+import { UserregistrationComponent } from './Users/userregistration/userregistration.component';
+import { LoginComponent } from './Users/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthInterceptor } from './auth.interceptor';
+import { UserServiceService } from './Shared/user-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    UserregistrationComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,HttpClientModule,ReactiveFormsModule 
   ],
-  providers: [],
+  providers: [UserServiceService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
